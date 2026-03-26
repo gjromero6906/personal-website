c2ad05e9efb785988f70ec0ea74dabda4cdaef38
gjromero6906 <gjr6906@gmail.com>
2026-03-26 16:42:29 -0400

    set up

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,3 +1,11 @@
 import './style.css'
 
+const logo = document.querySelector('.logo');
 
+logo.addEventListener('click', () => {
+  logo.classList.add('spring');
+
+  setTimeout(() => {
+    logo.classList.remove('spring');
+  }, 900);
+});
