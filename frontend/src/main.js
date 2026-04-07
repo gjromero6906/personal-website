d43f4e9e68c9ae73f0a3445859b2ddc6a4152f2a
gjromero6906 <gjr6906@gmail.com>
2026-04-07 19:46:33 -0400

    implimented feed back

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -13,3 +13,33 @@ const loadProjects = async () => {
   renderProjects(data);
 };
 loadProjects();
+const form = document.getElementById('contactForm');
+const thankYouMsg = document.getElementById('thankYouMsg');
+
+form.addEventListener('submit', async function(e) {
+    e.preventDefault(); // prevent page reload
+
+    const formData = new FormData(form);
+
+    try {
+        await fetch("https://formspree.io/f/xaqovkjy", {
+            method: "POST",
+            body: formData,
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+
+        thankYouMsg.textContent = "💚💚💚Thank you! Your message has been sent.💚💚💚";
+        thankYouMsg.classList.add("show");
+        form.reset();
+
+        setTimeout(() => {
+            thankYouMsg.classList.remove("show");
+        }, 5000);
+
+    } catch (error) {
+        thankYouMsg.textContent = "❌❌❌Oops! Something went wrong.❌❌❌";
+        thankYouMsg.classList.add("show");
+    }
+});
